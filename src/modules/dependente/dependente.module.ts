cde378976c2158c7e6c8daa113a04d5a2bc4da00
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2023-05-05 07:01:07 -0300

    revert: remove dependent

--- src/modules/dependente/dependente.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DependenteService } from './dependente.service';
-import { DependenteController } from './dependente.controller';
-
-@Module({
-  controllers: [DependenteController],
-  providers: [DependenteService],
-})
-export class DependenteModule {}
