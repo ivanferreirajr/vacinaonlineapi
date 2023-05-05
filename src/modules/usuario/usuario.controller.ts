6226bea49c10bd02f03714b2cb59130ebdd14651
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2023-05-05 08:21:00 -0300

    docs(swagger): add api property

--- src/modules/usuario/usuario.controller.ts
+++ src/modules/usuario/usuario.controller.ts
@@ -9,33 +9,41 @@ import {
 } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioDto } from './usuario.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a user', tags: ['User'] })
   create(@Body() createUsuarioDto: UsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all users', tags: ['User'] })
   findAll() {
     return this.usuarioService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a user by id', tags: ['User'] })
   findOne(@Param('id') id: string) {
     return this.usuarioService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user', tags: ['User'] })
   update(@Param('id') id: string, @Body() updateUsuarioDto: UsuarioDto) {
     return this.usuarioService.update(+id, updateUsuarioDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove a user', tags: ['User'] })
   remove(@Param('id') id: string) {
     return this.usuarioService.remove(+id);
   }
+
+  // TODO: rota para troca de senha
 }
--- src/modules/usuario/usuario.dto.ts
+++ src/modules/usuario/usuario.dto.ts
@@ -1,12 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UsuarioDto {
+  @ApiProperty()
   id?: number;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   senha: string;
+
+  @ApiProperty()
   cpf: string;
+
+  @ApiProperty()
   telefone?: string;
+
+  @ApiProperty()
   data_nascimento: Date;
+
+  @ApiProperty()
   coren?: string;
+
+  @ApiProperty()
   tipo?: string;
+
+  @ApiProperty()
   createdAt?: Date;
+
+  @ApiProperty()
   updatedAt?: Date;
 }
